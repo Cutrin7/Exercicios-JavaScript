f55d5c016f72370c6a26c1fbbeb3419be1f9d250
Cutrin <162223939+Cutrin7@users.noreply.github.com>
2024-09-05 21:22:00 -0300

    Add files via upload

--- JavaScript/js/script12.js
+++ JavaScript/js/script12.js
@@ -1,2 +1,3 @@
-var nome01=prompt('Digite seu nome: ')
+//EXERCICIO 1
+let nome01=prompt('Digite seu nome: ')
 alert('Bem vindo ' + nome01)
--- JavaScript/js/script13.js
+++ JavaScript/js/script13.js
@@ -1,6 +1,7 @@
-var numero01=parseInt(prompt('Digite um número: '))
-var numero02=parseInt(prompt('Digite um número: '))
+//EXERCICIO 2
+let numero01=parseInt(prompt('Digite um número: '))
+let numero02=parseInt(prompt('Digite um número: '))
 
-var resultado= numero01 + numero02
+let resultado= numero01 + numero02
 
 alert('A soma dos números é: '+ resultado)
